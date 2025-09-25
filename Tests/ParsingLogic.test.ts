798ed82a1d7845c01f0ad2b06a884e8807b98cae
Syed Ahmed <nadir.ahmed@live.co.uk>
2025-09-25 21:15:09 +0100

    feat: add 3le parser for happy path

--- Tests/ParsingLogic.test.ts
+++ Tests/ParsingLogic.test.ts
@@ -1,10 +1,10 @@
-import { Parse3leData } from '../logic/ParsingLogic';
+import { Parse3leData } from '../Logic/ParsingLogic';
 import * as fs from 'fs';
 
 describe('Testing ParsingLogic.Parse3leData', () => {
   test('Parse3leData', () => {
     const dataset = fs.readFileSync('./Tests/Datasets/2025-09-25_dataset_1.3le','utf-8');
     const expectedResult = JSON.parse(fs.readFileSync('./Tests/ExpectedData/2025-09-25_dataset_1.json', 'utf-8'));
-    expect(Parse3leData(dataset)).toBe(expectedResult);
+    expect(Parse3leData(dataset)).toEqual(expectedResult);
   });
 });
--- logic/ParsingLogic.ts
+++
@@ -1,5 +0,0 @@
-import { SatelliteData } from '../Models/SatelliteData';
-
-export function Parse3leData(data: string): SatelliteData[] {
-  return new Array<SatelliteData>();
-}
