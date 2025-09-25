6a25b8296964ee586d81023f8ced2d637f834c98
Syed Ahmed <nadir.ahmed@live.co.uk>
2025-09-25 20:16:35 +0100

    test: add test for happy path 3le parsing

--- Tests/ParsingLogic.test.ts
+++ Tests/ParsingLogic.test.ts
@@ -1,7 +1,10 @@
-import {Parse3leData} from '../logic/ParsingLogic';
+import { Parse3leData } from '../logic/ParsingLogic';
+import * as fs from 'fs';
 
 describe('Testing ParsingLogic.Parse3leData', () => {
   test('Parse3leData', () => {
-    expect(Parse3leData()).toBe()
-  })
-})
+    const dataset = fs.readFileSync('./Tests/Datasets/2025-09-25_dataset_1.3le','utf-8');
+    const expectedResult = JSON.parse(fs.readFileSync('./Tests/ExpectedData/2025-09-25_dataset_1.json', 'utf-8'));
+    expect(Parse3leData(dataset)).toBe(expectedResult);
+  });
+});
