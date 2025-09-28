2c3aa96020bc994de5dd49802b8168164cd50b07
Syed Ahmed <nadir.ahmed@live.co.uk>
2025-09-28 21:05:59 +0100

    feat: continue work on orbit determination

--- Logic/RenderLogic.ts
+++ Logic/RenderLogic.ts
@@ -1,56 +1,69 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Parse3le } from './ParsingLogic';
+import { SatelliteData } from '../Models/SatelliteData';
+import subsetData from '../Assets/2025-09-25_dataset_subset_5.3le?raw';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight,
-  2.5,
-  10,
+  3.5,
+  7.5,
 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
+
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enablePan = false;
+controls.enableZoom = false;
 
 const wireframeMaterial = new THREE.MeshDepthMaterial({
   wireframe: true,
   opacity: 1,
   visible: true,
-  side: THREE.FrontSide,
 });
 
-const circleGeo = new THREE.RingGeometry(1.25, 1.25, 32, 1);
+let satelliteData = Parse3le(subsetData);
+let satelliteOne: SatelliteData = satelliteData[0];
+
+const circleGeo1 = new THREE.RingGeometry(1.2756, 1.2756, 32, 1);
+circleGeo1.rotateY((Math.PI / 180) * 90);
+const circle1Material = new THREE.MeshStandardMaterial({
+  wireframe: true,
+  emissive: 0xff0000,
+  visible: true,
+});
+let circle1 = new THREE.LineLoop(circleGeo1, circle1Material);
+scene.add(circle1);
+
+const circleGeo = new THREE.RingGeometry(1.7, 1.7, 32, 1);
+// const ellipsePoints = new THREE.EllipseCurve(
+//   0,      // ax
+//   0,      // aY
+//   1.5,    // xRadius
+//   1.5,    // yRadius
+//   0,      // aStartAngle
+//   2 * Math.PI, // aEndAngle
+//   false,  // aClockwise
+//   0,      // aRotation
+// ).getPoints(100);
+// const circleGeo = new THREE.BufferGeometry().setFromPoints(ellipsePoints);
 circleGeo.rotateX((Math.PI / 180) * 90);
+circleGeo.rotateZ((Math.PI / 180) * satelliteOne.Inclination);
+
 let circle = new THREE.LineLoop(circleGeo, wireframeMaterial);
 scene.add(circle);
 
-const loader = new GLTFLoader();
-loader.load(
-  '../assets/Earth_v1.glb',
-  function (gltf) {
-    let model = gltf.scene;
-    model.traverse((obj) => {
-      if (obj.name === 'Earth') {
-        let wireframeObj = obj;
-        wireframeObj.material = wireframeMaterial;
-        scene.add(wireframeObj);
-      }
-    });
-  },
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-  },
-  function (error) {
-    console.error(error);
-  },
-);
+const sphereGeo = new THREE.SphereGeometry(1.2756, 32, 16);
+let sphere = new THREE.Mesh(sphereGeo, wireframeMaterial);
+scene.add(sphere);
 
-camera.position.z = 4;
-controls.update()
+camera.position.z = 5.5;
+controls.update();
 
 function animate() {
   requestAnimationFrame(animate);
--- readme.md
+++ readme.md
@@ -11,11 +11,14 @@ Written in TypeScript using ThreeJS to handle the OpenGL implemenation. Jest is 
 ## Learnings
 This section will be used to track things that I have learnt during the development of this project.
 
-- Learning to parse 3LE
+- Learn to parse 3LE
     - There is a possible downside with the current implmentation where if the 3le file is large enough more data than necessary is being loaded into memory. Which could be avoided by reading the data lines by line.
+- Learn TLE orbit propagation / determination
 - Learn shaders for render effects
 
 ## Useful Documentation
 
 - [3le Documentation SpaceTrack](https://www.space-track.org/documentation#tle-alpha5)
 - [Two Line Element Wiki](https://en.wikipedia.org/wiki/Two-line_element_set)
+- [TLE orbit determination using simplex method](https://www.sciencedirect.com/science/article/pii/S1674984723000368)
+- [An open-source solution for TLE based orbit determination](https://conference.sdo.esoc.esa.int/proceedings/sdc8/paper/10/SDC8-paper10.pdf)
